f79f69387faa1373e4b1e5bd1c5555336d64d05b
PavFet <fetiskinpavel@gmail.com>
2023-02-20 11:46:41 +0200

    feat: api router

--- src/server.ts
+++ src/server.ts
@@ -4,17 +4,41 @@ import morgan from 'morgan';
 
 dotenv.config();
 
+// setup
+const server = express();
 const { SERVER_PORT, SERVER_DOMAIN } = process.env;
 
 if (SERVER_PORT === undefined || SERVER_DOMAIN === undefined) {
   throw new Error("Please define constants in '.env' file");
 }
 
-const server = express();
-
+// Midlewares
 server.use(morgan('tiny'));
 server.use(express.static('public'));
+server.use(express.json());
 
+// API Router
+const cars = [
+  { id: 1, title: 'BMW1' },
+  { id: 2, title: 'BMW2' },
+  { id: 3, title: 'BMW3' },
+  { id: 4, title: 'BMW4' },
+];
+const apiRouter = express.Router();
+server.use('/api', apiRouter);
+
+apiRouter.get('/cars', (req, res) => {
+  res.status(200).json(cars);
+});
+
+apiRouter.post('/cars', (req, res) => {
+  const { body } = req;
+  const newCar = { id: 5, title: body.title };
+  cars.push(newCar);
+  res.status(201).json(cars);
+});
+
+// Server init
 server.listen(SERVER_PORT, () => {
   console.log(`server is running on:  http://${SERVER_DOMAIN}:${SERVER_PORT}`);
 });
